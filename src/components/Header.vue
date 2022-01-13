cfe5f97a81f48eb464f3b2ca67cf85b73c06a83c
AndrianiClaudio <claudioandriani1994@gmail.com>
2022-01-13 15:20:50 +0100

    inizio Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header>
+        <img :src='logo' :alt='alt'>
+    </header>
+</template>
+
+<script>
+export default ({
+    name: 'Header',
+    data() {
+        return {
+            logo: '../assets/dc-logo.png',
+            alt:'Logo DC'
+        }
+    }
+})
+</script>
+
+<style scoped lang = 'scss'>
+    
+</style>
